ee10b67c88612cb46ae7e002cdccb28a556cfdc9
akurtaliqi <a.kurtaliqi@gmail.com>
2015-12-10 08:16:14 +0100

    Classes business (terminées).

--- AKU-MovieManager/src/java/ch/hearc/ig/odi/moviemanager/business/Movie.java
+++ AKU-MovieManager/src/java/ch/hearc/ig/odi/moviemanager/business/Movie.java
@@ -10,5 +10,43 @@ package ch.hearc.ig.odi.moviemanager.business;
  * @author ajtene.kurtaliq
  */
 public class Movie {
+    private Long id;
+    private String name;
+    private String producer;
+
+    public Movie() {
+    }
+
+    public Movie(Long id, String name, String producer) {
+        this.id = id;
+        this.name = name;
+        this.producer = producer;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getProducer() {
+        return producer;
+    }
+
+    public void setProducer(String producer) {
+        this.producer = producer;
+    }
+    
+    
     
 }
