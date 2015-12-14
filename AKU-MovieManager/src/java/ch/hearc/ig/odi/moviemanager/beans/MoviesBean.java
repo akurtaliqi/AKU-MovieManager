b484c1f68405ac61ba6a9a0c9c9f727ae7575fa2
ajtene.kurtaliq <ajtene.kurtaliq@IG14-KURTALIQIA.intra.eiaj.ch>
2015-12-14 16:42:04 +0100

    index.xhtml fonctionnel (persons et movies) + ajout MoviesBean

---
+++ AKU-MovieManager/src/java/ch/hearc/ig/odi/moviemanager/beans/MoviesBean.java
@@ -0,0 +1,35 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ch.hearc.ig.odi.moviemanager.beans;
+
+import ch.hearc.ig.odi.moviemanager.business.Movie;
+import ch.hearc.ig.odi.moviemanager.services.Services;
+import java.util.List;
+import javax.enterprise.context.RequestScoped;
+import javax.inject.Inject;
+import javax.inject.Named;
+
+/**
+ *
+ * @author ajtene.kurtaliq
+ */
+@Named(value = "moviesBean")
+@RequestScoped
+public class MoviesBean {
+
+    @Inject
+    Services services;
+
+    /**
+     * Get a list of people
+     *
+     * @return list of people
+     */
+    public List<Movie> getMovies() {
+        return services.getMoviesList();
+    }
+
+}
